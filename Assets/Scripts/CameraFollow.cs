32425c4dd75d01c01baeb9c6b48abc979b29e2a4
cut-it-out <43676292+cut-it-out@users.noreply.github.com>
2021-01-28 02:19:16 +0100

    update cameraFollow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] Transform lookAtTarget;
+
+
+    [SerializeField] Vector3 targetOffset;
+    [SerializeField] Vector3 lookAtOffset;
 
     [SerializeField] float smoothSpeed = 2f;
-    [SerializeField] Vector3 offset;
 
     private void LateUpdate()
     {
-        Vector3 desiredPos = target.position + offset;
+        Vector3 desiredPos = target.position + targetOffset;
         Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPos;
-        
+
+        transform.LookAt(lookAtTarget.position + lookAtOffset);
+
     }
 }
